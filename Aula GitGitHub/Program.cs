9ddbecab02af3edadbfb0b384651e885e8b032be
Bernardo <berdavoglio@gmail.com>
2020-04-29 16:55:55 -0300

    Projeto esta mostrando Olá Mundo!

---
+++ Aula GitGitHub/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Aula_GitGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo!");
+            Console.WriteLine();
+        }
+    }
+}
